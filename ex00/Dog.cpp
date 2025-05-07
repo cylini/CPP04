e182eadb005584fc9513def6181c68d31a71d05f
Carine Zhang <carzhang@paul-f4Br7s3.clusters.42paris.fr>
2025-05-07 16:20:03 +0200

    ex00 ok , mieux comprendre et des notes

---
+++ ex00/Dog.cpp
@@ -0,0 +1,29 @@
+#include "Dog.hpp"
+
+Dog::Dog()
+{
+	std::cout << "Dog default constructor called" << std::endl;
+	_type = "Dog";
+}
+Dog::Dog(const Dog &copy) : Animal(copy)
+{
+	std::cout << "Dog copy constructor called" << std::endl;
+	*this = copy;
+	// _type = copy._type;
+}
+Dog::~Dog()
+{
+	std::cout << "Dog destructor called" << std::endl;
+}
+Dog &Dog::operator=(const Dog &copy)
+{
+	if (this != &copy)
+	{
+		_type = copy._type;
+	}
+	return *this;
+}
+void Dog::makeSound() const
+{
+	std::cout << "Dog " << _type << " makes sound : Woof !" << std::endl;
+}
