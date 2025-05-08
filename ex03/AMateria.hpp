0e7a44c1b35d896d91cbb40fdd8761f803958122
carzhang <carzhang@student.42.fr>
2025-05-08 23:17:35 +0200

    commence ex03

---
+++ ex03/AMateria.hpp
@@ -0,0 +1,24 @@
+#ifndef AMATERIA_HPP
+#define AMATERIA_HPP
+#include <iostream>
+
+#include "ICharacter.hpp"
+
+class AMateria
+{
+protected:
+	std::string _type;
+
+public:
+	AMateria(const std::string const &type);
+	AMateria(AMateria const &src);
+	AMateria &operator=(AMateria const &copy);
+	virtual ~AMateria();
+	AMateria();
+
+	std::string const getType() const; // Returns the materia type
+	virtual AMateria *clone() const = 0;
+	virtual void use(ICharacter &target);
+};
+
+#endif
---
+++ ex03/MateriaSource.hpp
@@ -0,0 +1,23 @@
+#ifndef MATERIASOURCE_HPP
+#define MATERIASOURCE_HPP
+
+#include "IMateriaSource.hpp"
+#include "AMateria.hpp"
+
+class MateriaSource : public IMateriaSource
+{
+private:
+	AMateria *_materia[4];
+	int _count;
+
+public:
+	MateriaSource();
+	MateriaSource(MateriaSource const &src);
+	MateriaSource &operator=(MateriaSource const &copy);
+	/* virtual */ ~MateriaSource();
+
+	void learnMateria(AMateria *m);
+	AMateria *createMateria(std::string const &type);
+};
+
+#endif
